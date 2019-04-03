a48df8e7a7916ff7e6225cc1fc5910ef8c53159c
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2019-04-03 17:43:31 -0400

    updates

--- src/main/java/com/ewei/parquet/App.java
+++ src/main/java/com/ewei/parquet/App.java
@@ -4,29 +4,60 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.parquet.bytes.*;
 import org.apache.parquet.column.values.delta.*;
+import org.apache.parquet.hadoop.CodecFactory;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 
 
 public class App {
+    private static final CodecFactory CODEC_FACTORY = new CodecFactory(new Configuration(), 1024);
+    private static final CodecFactory.BytesInputCompressor GZIP_COMPRESSOR =
+            CODEC_FACTORY.getCompressor(CompressionCodecName.GZIP);
+    private static final CodecFactory.BytesInputCompressor SNAPPY_COMPRESSOR =
+            CODEC_FACTORY.getCompressor(CompressionCodecName.SNAPPY);
+
     public static void main(String[] args) {
+        // Original data
+//        File initialFile = new File("/Users/elizabethwei/code/parquet_benchmark/src/main/java/com/ewei/parquet/sample.txt");
+//        try {
+//            // Encoded, snappy setup
+//            System.out.println(initialFile.length());
+//            InputStream targetStream = new FileInputStream(initialFile);
+//            BytesInput input = BytesInput.from(targetStream, (int) initialFile.length());
+//
+//
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+        // Encoded, un-compressed setup
         DirectByteBufferAllocator bytesAllocator = DirectByteBufferAllocator.getInstance();
         int slabSize = 256;
         int pageSize = 1000;
-
         DeltaBinaryPackingValuesWriterForInteger deltaBinaryPackingValuesWriterForInteger =
                 new DeltaBinaryPackingValuesWriterForInteger(slabSize, pageSize, bytesAllocator);
-        List<Byte> plainNumberBytes = new ArrayList<Byte>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i <= 1e6; i++) {
             deltaBinaryPackingValuesWriterForInteger.writeInteger(i);
-            plainNumberBytes.add(new Integer(i).byteValue());
         }
 
         try {
             byte[] encodedBytes = deltaBinaryPackingValuesWriterForInteger.getBytes().toByteArray();
             System.out.println("encoded length: " + encodedBytes.length);
-            System.out.println("previous length: " + plainNumberBytes.size());
+
+            BytesInput input = BytesInput.from(encodedBytes);
+
+            BytesInput snappy = SNAPPY_COMPRESSOR.compress(input);
+            System.out.println("snappy size: " + snappy.size());
+
+            BytesInput gzip = GZIP_COMPRESSOR.compress(input);
+            System.out.println("gzip size: " + snappy.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
