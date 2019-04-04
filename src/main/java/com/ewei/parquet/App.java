f2648277029ebeffcf1b47d9b75852742973083f
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2019-04-04 17:51:45 -0400

    updates

--- src/main/java/com/ewei/parquet/App.java
+++ src/main/java/com/ewei/parquet/App.java
@@ -1,8 +1,6 @@
 package com.ewei.parquet;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.parquet.bytes.*;
@@ -13,54 +11,51 @@ import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 
 public class App {
     private static final CodecFactory CODEC_FACTORY = new CodecFactory(new Configuration(), 1024);
-    private static final CodecFactory.BytesInputCompressor GZIP_COMPRESSOR =
-            CODEC_FACTORY.getCompressor(CompressionCodecName.GZIP);
-    private static final CodecFactory.BytesInputCompressor SNAPPY_COMPRESSOR =
-            CODEC_FACTORY.getCompressor(CompressionCodecName.SNAPPY);
+    private static final CodecFactory.BytesInputCompressor GZIP_COMPRESSOR =  CODEC_FACTORY.getCompressor(CompressionCodecName.GZIP);
+    private static final CodecFactory.BytesInputCompressor SNAPPY_COMPRESSOR = CODEC_FACTORY.getCompressor(CompressionCodecName.SNAPPY);
 
     public static void main(String[] args) {
-        // Original data
-//        File initialFile = new File("/Users/elizabethwei/code/parquet_benchmark/src/main/java/com/ewei/parquet/sample.txt");
-//        try {
-//            // Encoded, snappy setup
-//            System.out.println(initialFile.length());
-//            InputStream targetStream = new FileInputStream(initialFile);
-//            BytesInput input = BytesInput.from(targetStream, (int) initialFile.length());
-//
-//
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-        // Encoded, un-compressed setup
-        DirectByteBufferAllocator bytesAllocator = DirectByteBufferAllocator.getInstance();
+        // Set up delta binary packing encoder
         int slabSize = 256;
         int pageSize = 1000;
-        DeltaBinaryPackingValuesWriterForInteger deltaBinaryPackingValuesWriterForInteger =
-                new DeltaBinaryPackingValuesWriterForInteger(slabSize, pageSize, bytesAllocator);
+        DirectByteBufferAllocator bytesAllocator = DirectByteBufferAllocator.getInstance();
+        DeltaBinaryPackingValuesWriterForInteger deltaBinaryPackingValuesWriterForInteger = new DeltaBinaryPackingValuesWriterForInteger(slabSize, pageSize, bytesAllocator);
 
-        for (int i = 1; i <= 1e6; i++) {
-            deltaBinaryPackingValuesWriterForInteger.writeInteger(i);
+        File columnFile = new File("/Users/elizabethwei/code/parquet_benchmark/src/main/java/com/ewei/parquet/l_shipdate");
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(columnFile));
+            try {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    line = line.replaceAll("\\D", "");
+                    deltaBinaryPackingValuesWriterForInteger.writeInteger(Integer.parseInt(line));
+                }
+            } finally {
+                br.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         try {
+            // Get encoded size
             byte[] encodedBytes = deltaBinaryPackingValuesWriterForInteger.getBytes().toByteArray();
             System.out.println("encoded length: " + encodedBytes.length);
 
             BytesInput input = BytesInput.from(encodedBytes);
 
+            // Get snappy size
             BytesInput snappy = SNAPPY_COMPRESSOR.compress(input);
             System.out.println("snappy size: " + snappy.size());
 
+            // Get gzip size
             BytesInput gzip = GZIP_COMPRESSOR.compress(input);
-            System.out.println("gzip size: " + snappy.size());
+            System.out.println("gzip size: " + gzip.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
